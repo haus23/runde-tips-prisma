2979dc2009407ff64732bfc1089033c9789cc1b4
Micha Buchholz <mail@lean-stack.de>
2023-08-06 13:11:04 +0200

    feat: Secure manager with remix-auth.

--- app/routes/manager+/_index/_dashboard.tsx
+++ app/routes/manager+/_index/_dashboard.tsx
@@ -1,3 +1,12 @@
+import { type LoaderArgs } from "@remix-run/node";
+import { authenticator } from "~/utils/server/auth.server";
+
+export async function loader({ request }: LoaderArgs) {
+  await authenticator.isAuthenticated(request, {
+    failureRedirect: "/login",
+  });
+}
+
 export default function ManagerDashboardRoute() {
   return (
     <div>
---
+++ app/utils/server/auth.server.ts
@@ -0,0 +1,7 @@
+import { Authenticator } from "remix-auth";
+import { sessionStorage } from "./session.server";
+
+export const authenticator = new Authenticator<string>(sessionStorage, {
+  sessionKey: "sessionId",
+  throwOnError: true,
+});
